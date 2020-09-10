e6683e9c1c7d2eaa6d45c2e19492604564068978
TB <snorka@live.de>
2020-09-10 22:21:00 +0200

    Merge

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    iconfont: 'md',
+    theme: {
+        darkblue: '#241c24',
+        green: "#315458",
+        orange: '#B1493C',
+    }
 });
