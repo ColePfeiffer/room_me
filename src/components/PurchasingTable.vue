5647607a8dc93ea51564befdf8fd144045aaec7f
TB <snorka@live.de>
2020-10-15 16:28:05 +0200

    added draggable, modified table in purchasingPage

--- src/components/PurchasingTable.vue
+++ src/components/PurchasingTable.vue
@@ -2,54 +2,65 @@
   <v-col xs="12" sm="12" md="6">
     <v-card flat class="pa-3">
       <div class="overline">Shopping List</div>
+
+      <draggable
+        v-if="false"
+        v-model="shoppingList2"
+        group="people"
+        @start="drag = true"
+        @end="drag = false"
+      >
+        <div v-for="element in shoppingList2" :key="element.article">{{ element.article }}</div>meh ----
+      </draggable>
+
       <v-simple-table>
         <template v-slot:default>
           <thead>
             <tr>
               <th class="text-left">name</th>
-              <v-tooltip top>
-                <template v-slot:activator="{ on, attrs }">
-                  <th class="text-left" v-bind="attrs" v-on="on">accept</th>
-                </template>
-                <span>Accepting</span>
-              </v-tooltip>
-              <v-tooltip top>
-                <template v-slot:activator="{ on, attrs }">
-                  <th class="text-left" v-bind="attrs" v-on="on">cash up</th>
-                </template>
-                <span>Was bedeutet Abrechnen?</span>
-              </v-tooltip>
-              <v-tooltip top>
-                <template v-slot:activator="{ on, attrs }">
-                  <th class="text-left" v-bind="attrs" v-on="on">edit</th>
-                </template>
-                <span>Meeeeeeeeeeta</span>
-              </v-tooltip>
+              <th class="text-left">actions</th>
             </tr>
           </thead>
-          <!--Shopping List Overview: Shows all products (should show only with status 0! -->
 
           <tbody :class="`pl-3 shoppingList ${shoppingList.status}`">
             <tr v-for="item in openItems" :key="item.article">
-              <PurchasingDialogEditItem
-                :item="item"
-                @save-changes="saveChangesInEditPage"
-              ></PurchasingDialogEditItem>
+              <PurchasingDialogEditItem :item="item" @save-changes="saveChangesInEditPage"></PurchasingDialogEditItem>
               <td>{{ item.article }}</td>
               <td>
-                <v-btn text>
-                  <v-icon @click="acceptItem(item)">mdi-check</v-icon>
-                </v-btn>
-              </td>
-              <td>
-                <v-btn text>
-                  <v-icon @click="cashUpItem(item)">euro</v-icon>
-                </v-btn>
-              </td>
-              <td>
-                <v-btn text>
-                  <v-icon @click="item.showEditDialog = true">edit</v-icon>
-                </v-btn>
+                <div>
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
+                      <th class="text-left" v-bind="attrs" v-on="on">
+                        <v-btn text>
+                          <v-icon @click="acceptItem(item)">mdi-check</v-icon>
+                        </v-btn>
+                      </th>
+                    </template>
+                    <span>You are gonna buy this? Awesome!</span>
+                  </v-tooltip>
+
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
+                      <th class="text-left" v-bind="attrs" v-on="on">
+                        <v-btn text>
+                          <v-icon @click="cashUpItem(item)">euro</v-icon>
+                        </v-btn>
+                      </th>
+                    </template>
+                    <span>Already bought this?</span>
+                  </v-tooltip>
+
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
+                      <th class="text-left" v-bind="attrs" v-on="on">
+                        <v-btn text>
+                          <v-icon @click="item.showEditDialog = true">edit</v-icon>
+                        </v-btn>
+                      </th>
+                    </template>
+                    <span>edit/delete</span>
+                  </v-tooltip>
+                </div>
               </td>
             </tr>
           </tbody>
@@ -60,9 +71,7 @@
         <v-row>
           <v-col md="12" sm="12" xs="12">
             <!--Appears if shoppingList is empty!-->
-            <p v-if="openItems.length === 0">
-              No supplies added yet. Please start adding something.
-            </p>
+            <p v-if="openItems.length === 0">No supplies added yet. Please start adding something.</p>
             <v-text-field
               v-on:keydown.enter="addItem"
               v-model="submittedItem"
@@ -78,6 +87,7 @@
 
 <script>
 import PurchasingDialogEditItem from "./PurchasingDialogEditItem";
+import draggable from "vuedraggable";
 
 export default {
   name: "PurchasingTable",
@@ -87,7 +97,7 @@ export default {
     currencySymbol: String,
     newPurchase: Object
   },
-  components: { PurchasingDialogEditItem },
+  components: { PurchasingDialogEditItem, draggable },
   computed: {
     // Wählt einzig die aktiven Items aus der ShoppingList aus, um diese anzuzeigen
     openItems() {
@@ -114,7 +124,58 @@ export default {
       // Supply: Item that User enters for shoppingList:
       submittedItem: "",
       completedPurchase: false,
-      currentItemForCashingUp: {}
+      currentItemForCashingUp: {},
+      shoppingList2: [
+        {
+          article: "Spüli",
+          // Status: 0 - offen, 1 - pending,
+          price: 0,
+          status: 0,
+          acceptedBy: "",
+          boughtBy: "",
+          declinedBy: []
+        },
+        {
+          article: "Müllsäcke",
+          price: 0,
+          status: 0,
+          acceptedBy: "",
+          boughtBy: "",
+          declinedBy: []
+        },
+        {
+          article: "Ingwerbröd",
+          price: 0,
+          status: 0,
+          acceptedBy: "",
+          boughtBy: "",
+          declinedBy: []
+        },
+        {
+          article: "Aluhut",
+          price: 0,
+          status: 1,
+          acceptedBy: "",
+          boughtBy: "",
+          declinedBy: []
+        },
+        {
+          article: "Seife",
+          price: 0,
+          status: 1,
+          acceptedBy: "",
+          boughtBy: "",
+          declinedBy: []
+        },
+        {
+          article: "Wlan Repeater",
+          price: 2.7,
+          status: 99,
+          acceptedBy: "",
+          boughtBy: "",
+          declinedBy: []
+        }
+      ]
     };
   },
   methods: {
